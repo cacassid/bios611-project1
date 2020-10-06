c1c311ae359994cc9ea13469c66a1e1a604d0717
Caitlin Cassidy <cacassid@email.unc.edu>
2020-10-06 15:38:31 -0400

    Created mammals vs vascular plants plot

--- scatter_figures.R
+++ scatter_figures.R
@@ -3,6 +3,7 @@ library(tidyverse)
 counts_area_sl <- read_csv("./derived_data/counts_area_sl.csv")
 
 #scatterplots
+#Mammals vs Birds
 scatter1 <- ggplot(counts_area_sl, aes(x=Mammal, y=Bird, size = Acres)) + geom_point(alpha = 1, color = "#0A684A") + 
   xlab("Number of Mammal Species") + ylab("Number of Bird Species") + 
   ggtitle("Number of Mammal Species vs Bird Species") +
@@ -10,6 +11,7 @@ scatter1 <- ggplot(counts_area_sl, aes(x=Mammal, y=Bird, size = Acres)) + geom_p
   theme(legend.position = "none")
 scatter1
 
+#Mammals vs Fish
 scatter2 <- ggplot(counts_area_sl, aes(x=Mammal, y=Fish, size = Acres)) + geom_point(alpha = 1, color = "#1375A0") + 
   xlab("Number of Mammal Species") + ylab("Number of Fish Species") + 
   ggtitle("Number of Mammal Species vs Fish Species") +
@@ -17,11 +19,22 @@ scatter2 <- ggplot(counts_area_sl, aes(x=Mammal, y=Fish, size = Acres)) + geom_p
   theme(legend.position = "none")
 scatter2
 
+#Mammals vs Vascular Plants
+scatter3 <- ggplot(counts_area_sl, aes(x=Mammal, y=`Vascular Plant`, size = Acres)) + geom_point(alpha = 1, color = "#B09908") + 
+  xlab("Number of Mammal Species") + ylab("Number of Vascular Plant Species") + 
+  ggtitle("Number of Mammal Species vs Vascular Plant Species") +
+  geom_smooth(method=lm, se=FALSE, color = "black", alpha = 0.1) +
+  theme(legend.position = "none")
+scatter3
+
 ggsave("figures/scatter1.png",scatter1)
 saveRDS(scatter1, "figures/scatter1.rds")
 
 ggsave("figures/scatter2.png",scatter2)
 saveRDS(scatter2, "figures/scatter2.rds")
+
+ggsave("figures/scatter3.png",scatter3)
+saveRDS(scatter3, "figures/scatter3.rds")
 
 
 
