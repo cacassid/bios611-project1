2e50a7f96d7e17618af6c8aa1bb397ae9deba908
Caitlin Cassidy <cacassid@email.unc.edu>
2020-10-06 16:56:13 -0400

    created glm figure 2

--- glm_figures.R
+++ glm_figures.R
@@ -63,8 +63,8 @@ glm_figure <- ggarrange(rsquare, cp, aic,
 glm_figure
 
 #combined figure of model plots
-model_final <- lm(Mammal ~ Amphibian + Bird + Fish + Longitude, 
-                  data = counts_area_sl)
+model_final <- glm(formula = Mammal ~ Bird + Fish + Amphibian + Longitude, data = counts_area_sl)
+model_final
 
 amphibian <- effect_plot(model_final, pred = Amphibian, interval = TRUE, plot.points = TRUE)
 bird <- effect_plot(model_final, pred = Bird, interval = TRUE, plot.points = TRUE)
@@ -75,30 +75,7 @@ glm_figure2 <- ggarrange(amphibian, bird, fish, longitude,
                         ncol = 2, nrow = 2)
 glm_figure2
 
-
-
-
-#Forward selection
-forward <- ols_step_forward_p(model)
-forward
-#Variables included: bird, fish, amphibian, longitude, spider/scorpion, 
-#reptile, vascular plant
-
-#Backward selection
-backward <- ols_step_backward_p(model)
-backward
-#Variables included: 
-
-#Stepwise selection
-stepwise <- ols_step_both_p(model)
-stepwise
-#Variables included: bird, fish, amphibian, longitude
-fit_step <- glm(Mammal~Bird+Fish+Amphibian+Longitude,data=counts_area_sl)
-summary(fit_step)
-step_bird <- effect_plot(fit_step, pred = Bird, interval = TRUE, plot.points = TRUE)
-step_fish <- effect_plot(model, pred = Fish, interval = TRUE, plot.points = TRUE)
-step_amphibian <- effect_plot(model, pred = Amphibian, interval = TRUE, plot.points = TRUE)
-effect_plot(model, pred = Longitude, interval = TRUE, plot.points = TRUE)
+#create table of model info
 
 
 
@@ -106,5 +83,5 @@ effect_plot(model, pred = Longitude, interval = TRUE, plot.points = TRUE)
 ggsave("figures/glm_figure.png",glm_figure)
 saveRDS(glm_figure, "figures/glm_figure.rds")
 
-ggsave("figures/glm_figure2.png",glm_figure)
-saveRDS(glm_figure, "figures/glm_figure2.rds")
+ggsave("figures/glm_figure2.png",glm_figure2)
+saveRDS(glm_figure2, "figures/glm_figure2.rds")
