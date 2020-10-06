18b07ef6a5168b2c834f93b4b9142f2ed714e5dc
Caitlin Cassidy <cacassid@email.unc.edu>
2020-10-06 18:18:24 -0400

    modified figures to only show top 20

--- dot_figures.R
+++ dot_figures.R
@@ -5,15 +5,11 @@ conservation_park_info <- read_csv("./derived_data/conservation_park_info.csv")
 #dot plot
 conservation_park_info2 <- conservation_park_info[order(conservation_park_info$Endangered), ]
 conservation_park_info2$Park.Name <- factor(conservation_park_info2$Park.Name, levels = conservation_park_info2$Park.Name) 
+conservation_park_info2 <- conservation_park_info2[37:56, ]
 
 dot_plot_e <- ggplot(conservation_park_info2, aes(x=Park.Name, y=Endangered)) + 
   geom_point(col="red", size=3) + 
-  geom_segment(aes(x=Park.Name, 
-                   xend=Park.Name, 
-                   y=min(Endangered), 
-                   yend=max(Endangered)), 
-               linetype="dashed", 
-               size=0.1) +   
+  ylim(0, 50) +
   xlab("Park") + ylab("Number of Species") +
   labs(title="Endangered Species in US National Parks") +  
   coord_flip()
@@ -21,15 +17,11 @@ dot_plot_e
 
 conservation_park_info3 <- conservation_park_info[order(conservation_park_info$`Species of Concern`), ]
 conservation_park_info3$Park.Name <- factor(conservation_park_info3$Park.Name, levels = conservation_park_info3$Park.Name)
+conservation_park_info3 <- conservation_park_info3[37:56, ]
 
 dot_plot_c <- ggplot(conservation_park_info3, aes(x=Park.Name, y=`Species of Concern`)) + 
   geom_point(col="orange", size=3) + 
-  geom_segment(aes(x=Park.Name, 
-                   xend=Park.Name, 
-                   y=min(`Species of Concern`), 
-                   yend=max(`Species of Concern`)), 
-               linetype="dashed", 
-               size=0.1) +   
+  ylim(0, 180) +
   xlab("Park") + ylab("Number of Species") +
   labs(title="Species of Concern in US National Parks") +  
   coord_flip()
