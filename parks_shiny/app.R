da16b51e8f8787e0d239f0515f94260575b233ae
Caitlin Cassidy <cacassid@email.unc.edu>
2020-10-28 16:30:50 -0400

    updated histograms

--- parks_shiny/app.R
+++ parks_shiny/app.R
@@ -25,7 +25,13 @@ ui <- fluidPage(
                          "Which variable to display",
                          choices = c("Mammal" = "Mammal",
                                      "Bird" = "Bird",
-                                     "Reptile" = "Reptile"),
+                                     "Reptile" = "Reptile", 
+                                     "Fish" = "Fish", 
+                                     "Amphibian" = "Amphibian",
+                                     "Insect" = "Insect",
+                                     "Fungi" = "Fungi",
+                                     "Vascular Plants" = "Vascular Plants",
+                                     "Nonvascular Plants" = "Nonvascular Plants"),
                          selected = "Mammal"
             ),
         ),
@@ -44,7 +50,7 @@ server <- function(input, output) {
         # set x-axis label depending on the value of display_var
         if (input$display_var == "Mammal") {
             ggplot(counts_area_sl, aes(x=LatCat2, y = Mammal)) + 
-                geom_bar(stat = "identity", fill = "#03783D") +
+                geom_bar(stat = "identity", fill = "#4A5C93") +
                 xlab("Latitude") + ylab("Number of Mammal Species")
         } else if (input$display_var == "Bird") {
             ggplot(counts_area_sl, aes(x=LatCat2, y = Bird)) + 
@@ -54,6 +60,32 @@ server <- function(input, output) {
             ggplot(counts_area_sl, aes(x=LatCat2, y = Reptile)) + 
                 geom_bar(stat = "identity", fill = "#86C14C") +
                 xlab("Latitude") + ylab("Number of Reptile Species")
+        } else if (input$display_var == "Fish") {
+            ggplot(counts_area_sl, aes(x=LatCat2, y = Fish)) + 
+                geom_bar(stat = "identity", fill = "#1E81CA") +
+                xlab("Latitude") + ylab("Number of Fish Species")
+        } else if (input$display_var == "Amphibian") {
+            ggplot(counts_area_sl, aes(x=LatCat2, y = Amphibian)) + 
+                geom_bar(stat = "identity", fill = "#CA6F1E") +
+                xlab("Latitude") + ylab("Number of Amphibian Species")
+        } else if (input$display_var == "Insect") {
+            ggplot(counts_area_sl, aes(x=LatCat2, y = Insect)) + 
+                geom_bar(stat = "identity", fill = "#CA381E") +
+                xlab("Latitude") + ylab("Number of Insect Species")
+        } else if (input$display_var == "Fungi") {
+            ggplot(counts_area_sl, aes(x=LatCat2, y = Fungi)) + 
+                geom_bar(stat = "identity", fill = "#976F44") +
+                xlab("Latitude") + ylab("Number of Fungi Species")
+        } 
+        else if (input$display_var == "Vascular Plants") {
+            ggplot(counts_area_sl, aes(x=LatCat2, y = `Vascular Plant`)) + 
+                geom_bar(stat = "identity", fill = "#03783D") +
+                xlab("Latitude") + ylab("Number of Vascular Plant Species")
+        } 
+        else if (input$display_var == "Nonvascular Plants") {
+            ggplot(counts_area_sl, aes(x=LatCat2, y = `Nonvascular Plant`)) + 
+                       geom_bar(stat = "identity", fill = "#6A934A") +
+                       xlab("Latitude") + ylab("Number of Nonvascular Plant Species")
         } 
         
     })
